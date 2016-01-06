96be4df2e591fc585c0e0e2113fb6ace7f1dc82b
Aron Chudnoff <chudnoff@gmail.com>
2016-01-05 23:44:33 -0600

    Hello World!

--- GigaBot/Program.cs
+++ GigaBot/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using Discord.Commands.Permissions.Levels;
+using Discord.Commands.Permissions.Userlist;
+using Discord.Modules;
+using System;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,23 @@ namespace GigaBot
     {
         static void Main(string[] args)
         {
+            var client = new DiscordClient();
+            var _login = new Login();
+
+
+
+
+            client.Run(async () =>
+            {
+                await client.Connect(_login.UserId, _login.Password);
+                Console.WriteLine("Logged In.");
+
+                Console.WriteLine(client.GetServer(131362574821359617));
+                Console.WriteLine(client.GetChannel(133959873766359040));
+                await client.SendMessage(client.GetChannel(133959873766359040), "Hello World!");
+
+            });
         }
     }
 }
+
